2897aa8e6c47383502fc4a5786d5d3fcd844debb
cmj <chmin82@gmail.com>
2024-08-11 22:29:25 +0900

    [FEAT] 드리프트 생성 및 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_calendar_scheduler/database/drift_database.dart';
 import 'package:flutter_calendar_scheduler/screen/home_screen.dart';
+import 'package:get_it/get_it.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
@@ -7,6 +9,10 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await initializeDateFormatting();
+
+  final database = LocalDatabase();
+
+  GetIt.I.registerSingleton<LocalDatabase>(database);
 
   runApp(MaterialApp(
     home: HomeScreen(),
