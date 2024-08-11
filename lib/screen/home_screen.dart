0000e6aa93e98d0653f8885700c171850d6e9bda
cmj <chmin82@gmail.com>
2024-08-11 13:55:25 +0900

    [FEAT] CustomTextField, ScheduleBottomSheet

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_calendar_scheduler/component/main_calendar.dart';
 import 'package:flutter_calendar_scheduler/component/schedule_card.dart';
 import 'package:flutter_calendar_scheduler/component/today_banner.dart';
+import 'package:flutter_calendar_scheduler/component/schedule_bottom_sheet.dart';
+import 'package:flutter_calendar_scheduler/const/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,22 +14,40 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   DateTime selectedDate = DateTime.utc(
-      DateTime.now().year, DateTime.now().month, DateTime.now().day);
+      DateTime
+          .now()
+          .year, DateTime
+      .now()
+      .month, DateTime
+      .now()
+      .day);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: PRIMARY_COLOR,
+          onPressed: () {
+            showModalBottomSheet(
+                context: context,
+                isDismissible: true,
+                builder: (_) {
+                  return ScheduleBottomSheet();
+                });
+          },
+          child: Icon(Icons.add),
+        ),
         body: SafeArea(
             child: Column(children: [
-      MainCalendar(
-        selectedDate: selectedDate,
-        onDaySelected: onDaySelected,
-      ),
-      SizedBox(height: 8.0),
-      TodayBanner(selectedDate: selectedDate, count: 0),
-      SizedBox(height: 8.0),
-      ScheduleCard(startTime: 12, endTime: 14, content: '프로그래밍 공부')
-    ])));
+              MainCalendar(
+                selectedDate: selectedDate,
+                onDaySelected: onDaySelected,
+              ),
+              SizedBox(height: 8.0),
+              TodayBanner(selectedDate: selectedDate, count: 0),
+              SizedBox(height: 8.0),
+              ScheduleCard(startTime: 12, endTime: 14, content: '프로그래밍 공부')
+            ])));
   }
 
   void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
