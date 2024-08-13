9b88065126e7c9e01dc8ed280382ebf82bae4ad0
cmj <chmin82@gmail.com>
2024-08-13 21:48:28 +0900

    [FEAT] 글로벌 상태 관리 ScheduleProvider

--- lib/provider/schedule_provider.dart
+++ lib/provider/schedule_provider.dart
@@ -16,11 +16,54 @@ class ScheduleProvider extends ChangeNotifier {
     getSchedules(date: selectedDate);
   }
 
+  // 조회
   void getSchedules({required DateTime date}) async {
     final resp = await repository.getSchedules(date: date);
     // 일정 업데이트
     cache.update(date, (value) => resp, ifAbsent: () => resp);
     // 리슨하는 위젯들 업데이트
+    notifyListeners();
+  }
+
+  // 생성
+  void createSchedule({required ScheduleModel schedule}) async {
+    final targetDate = schedule.date;
+    final savedSchedule = await repository.createSchedule(schedule: schedule);
+
+    cache.update(
+      targetDate,
+      (value) => [
+        ...value,
+        schedule.copyWith(
+          id: savedSchedule,
+        ),
+      ]..sort(
+          (a, b) => a.startTime.compareTo(
+            b.startTime,
+          ),
+        ),
+      ifAbsent: () => [schedule],
+    );
+    notifyListeners();
+  }
+
+  // 삭제
+  void deleteSchedules({
+    required DateTime date,
+    required String id,
+  }) async {
+    final resp = await repository.deleteSchedule(id: id);
+
+    cache.update(date, (value) => value.where((e) => e.id != id).toList(),
+        ifAbsent: () => []);
+    notifyListeners();
+  }
+
+  // 수정
+  void changeSelectedDate({
+    required DateTime date,
+  }) {
+    selectedDate = date;
     notifyListeners();
   }
 }
