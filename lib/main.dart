1de96f67094accf9231a7d24c24786a88e807426
cmj <chmin82@gmail.com>
2024-08-13 21:51:28 +0900

    [FEAT] 프로바이더 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_calendar_scheduler/database/drift_database.dart';
+import 'package:flutter_calendar_scheduler/provider/schedule_provider.dart';
+import 'package:flutter_calendar_scheduler/repository/schedule_repository.dart';
 import 'package:flutter_calendar_scheduler/screen/home_screen.dart';
 import 'package:get_it/get_it.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   // 플러터 프레임워크가 준비될 때까지 대기
@@ -14,7 +17,15 @@ void main() async {
 
   GetIt.I.registerSingleton<LocalDatabase>(database);
 
-  runApp(MaterialApp(
-    home: HomeScreen(),
-  ));
+  final repository = ScheduleRepository();
+  final scheudleProvider = ScheduleProvider(repository: repository);
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => scheudleProvider,
+      child: MaterialApp(
+        home: HomeScreen(),
+      ),
+    ),
+  );
 }
